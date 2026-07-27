16e52b438ed349ea05c126419815862216085bdb
Edward Fu <edwardfu@Edwards-MacBook-Pro.local>
2026-07-26 22:15:25 -0700

    Wire D4H live key via env var, fix US API endpoint, drop team ID requirement

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed "s/__GROQ_API_KEY__/${GROQ_API_KEY}/g" index.html > index.built.html
+sed "s/__GROQ_API_KEY__/${GROQ_API_KEY}/g" index.html \
+  | sed "s/__D4H_API_KEY__/${D4H_API_KEY}/g" > index.built.html
 mv index.built.html index.html
