7dcf057339a034aebb060a745e9c3b4752b012b9
Edward Fu <edwardfu@Edwards-MacBook-Pro.local>
2026-07-31 20:23:29 -0700

    Switch AI from Groq to Claude API (Haiku)

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed "s/__GROQ_API_KEY__/${GROQ_API_KEY}/g" index.html \
+sed "s/__CLAUDE_API_KEY__/${CLAUDE_API_KEY}/g" index.html \
   | sed "s/__D4H_API_KEY__/${D4H_API_KEY}/g" > index.built.html
 mv index.built.html index.html
